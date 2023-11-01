7f9fa089bbd0176e880a79709411160abd9e1d5b
tieckby <tiemokosogodogo97@gmail.com>
2023-11-01 15:17:59 +0000

    ...

---
+++ forms/contact.js
@@ -0,0 +1,31 @@
+function removeItems() {
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('subject').value = '';
+    document.getElementById('message').value = '';
+}
+
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function(e) {
+    e.preventDefault()
+    var nom = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+    var body = "Sujet : " + subject + "<br>" + "Nom complet : " + nom + "<br>" + "Email : " + email + "<br>" + "Message : " +message
+
+
+    Email.send({
+        Host: "",
+        Username: "",
+        Password: "",
+        To: '',
+        From: email,
+        Subject: subject,
+        Body: body,
+    }).then(
+        message => alert("Votre message a été envoyé. Merci !")
+    );
+
+    removeItems();
+});
